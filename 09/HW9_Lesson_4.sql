1ab0141219c241d74fc6e28e23943c53c3d943f3
Alexander <47240316+Savchuk18@users.noreply.github.com>
2019-05-28 15:31:07 +0500

    Add files via upload

---
+++ 09/HW9_Lesson_4.sql
@@ -0,0 +1,20 @@
+﻿/*
+4. Найти в StockItems строки, где есть тэг "Vintage"
+Запрос написать через функции работы с JSON.
+Тэги искать в поле CustomFields, а не в Tags.
+*/
+
+
+SELECT 
+	SI.*
+FROM [Warehouse].[StockItems] As SI
+	CROSS APPLY
+		OPENJSON(SI.CustomFields)
+			WITH(
+				CountryOfManufacture nvarchar(100),
+				Tags nvarchar(max)  as json
+			) AS Cust
+			outer apply openjson(Cust.Tags)
+						WITH( Tag nvarchar(15) '$')
+WHERE tag Like N'%Vintage%'
+
