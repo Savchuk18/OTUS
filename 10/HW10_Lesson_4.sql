fd3ca509979e897265605b43e59b8733d20d6131
Alexander <47240316+Savchuk18@users.noreply.github.com>
2019-06-07 18:09:14 +0500

    Add files via upload

---
+++ 10/HW10_Lesson_4.sql
@@ -0,0 +1,16 @@
+﻿
+/*
+Создайте табличную функцию покажите как ее можно вызвать для каждой строки result set'а без использования цикла. 
+*/
+
+SELECT  SC.CustomerName
+		,F.OrderID
+		,F.Summ
+FROM [Sales].[Customers] AS SC
+CROSS APPLY 
+	[dbo].[funcOrdersCustBegEnd] (SC.CustomerName, --SUBSTRING(SC.CustomerName,1,6)
+	'20130101', '20130201') AS F
+WHERE F.Summ > 10000
+
+
+
